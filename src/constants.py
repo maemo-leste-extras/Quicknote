3bba09e995f4c8c2627b10f5503a4dbb961e6d2e
Ed Page <eopage@byu.net>
2010-12-21 19:06:05 -0600

    Bump to 0.7.13-1

--- src/constants.py
+++ src/constants.py
@@ -3,7 +3,7 @@ import os
 __pretty_app_name__ = "Quicknote"
 __app_name__ = "quicknote"
 __version__ = "0.7.13"
-__build__ = 0
+__build__ = 1
 _data_path_ = os.path.join(os.path.expanduser("~"), ".quicknote")
 __app_magic__ = 0xdeadbeef
 _user_logpath_ = "%s/quicknote.log" % _data_path_
