0313221d9a8c5d48c7526db8d21069377b4d1354
Ed Page <eopage@byu.net>
2010-12-28 21:42:18 -0600

    Pulling in skeleton changes

--- src/constants.py
+++ src/constants.py
@@ -4,6 +4,7 @@ __pretty_app_name__ = "Quicknote"
 __app_name__ = "quicknote"
 __version__ = "0.7.13"
 __build__ = 1
-_data_path_ = os.path.join(os.path.expanduser("~"), ".quicknote")
 __app_magic__ = 0xdeadbeef
-_user_logpath_ = "%s/quicknote.log" % _data_path_
+_data_path_ = os.path.join(os.path.expanduser("~"), ".%s" % __app_name__)
+_user_logpath_ = "%s/%s.log" % (_data_path_, __app_name__)
+
